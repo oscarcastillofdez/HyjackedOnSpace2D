825e572d9e0b455c10ac057542a320b6508f75dc
oscarcastillofdez <oscarcastillofdez@gmail.com>
2024-02-05 20:27:25 +0100

    aaaa

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+def main():
+     
+    pygame.init()
+    pygame.display.set_caption("Hyjacked on Space")
+    screen = pygame.display.set_mode((650,650))
+    
+if __name__=="__main__":
+    main()
