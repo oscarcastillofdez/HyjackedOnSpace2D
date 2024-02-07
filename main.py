43445515e95ad87808e5329e18fd442b06d80879
juan <juan.villaverde.rodriguez@udc.es>
2024-02-07 17:40:09 +0100

    bola roja cambiada por el personaje

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 
 def main():
-     
     pygame.init()
     pygame.display.set_caption("Hyjacked on Space")
     screen = pygame.display.set_mode((1280, 720))
@@ -20,8 +19,13 @@ def main():
     JUMP_HEIGHT = 20
     jumpVelocity = JUMP_HEIGHT
 
-    #playerPos = X_POS, Y_POS
-    playerPos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 1.5)
+    BACKGROUND = pygame.image.load("Assets/space/spr_big_meteor.png")
+    standing = pygame.transform.scale(pygame.image.load("Assets/move1.png"), (100,100))
+
+    
+    player_rect = standing.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2))
+    #player_pos = X_POS, Y_POS
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
     while running:
         # poll for events
@@ -29,11 +33,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        # fill the screen with a color to wipe away anything from last frame
         screen.fill("blue")
+        screen.blit(standing, player_rect)
+        screen.blit(BACKGROUND, (0,0))
+        # fill the screen with a color to wipe away anything from last frame
 
-        pygame.draw.circle(screen, "red", playerPos, 30)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -69,6 +73,11 @@ def main():
             if jumpVelocity < -JUMP_HEIGHT:
                 jumping = False
                 jumpVelocity = JUMP_HEIGHT
+                
+
+        player_rect = standing.get_rect(center=player_pos)
+
+        player_rect = standing.get_rect(center=player_pos)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
