74869193e863e8b8fb252e30ea8ca036c23e5030
oscarcastillofdez <oscarcastillofdez@gmail.com>
2024-02-12 19:10:49 +0100

    Eliminados los .py de pruebas

--- mainAux.py
+++
@@ -1,9 +0,0 @@
-import pygame
-def main():
-     
-    pygame.init()
-    pygame.display.set_caption("Hyjacked on Space")
-    screen = pygame.display.set_mode((650,650))
-    
-if __name__=="__main__":
-    main()
