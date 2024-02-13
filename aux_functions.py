7e063f63edaa5e8e7a7542949308745c998f000a
juan <juan.villaverde.rodriguez@udc.es>
2024-02-13 22:53:26 +0100

    move_horizontal movido a funciones aux

--- aux_functions.py
+++ aux_functions.py
@@ -5,3 +5,15 @@ def draw_grid(screen):
         for line in range(0,10):
             pygame.draw.line(screen, (255, 255, 255), (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_HEIGTH))
             pygame.draw.line(screen, (255, 255, 255), (0, line * TILE_SIZE), (SCREEN_WIDTH, line * TILE_SIZE))
+
+def move_horizontal(pressed, movement, deltaTime):
+    if pressed:
+        if movement < MAX_VELOCITY:
+            movement += INERTIA
+        pressed = False
+    else: 
+        if movement > INERTIA - 1:
+            movement -= INERTIA
+        else:
+            movement = 0
+    return (pressed, movement, movement * deltaTime)
--- main.py
+++ main.py
@@ -8,20 +8,6 @@ from enemy import *
 from player import *
 from world import *
 
-
-def move_horizontal(pressed, movement, deltaTime):
-    if pressed:
-        if movement < MAX_VELOCITY:
-            movement += INERTIA
-        pressed = False
-    else: 
-        if movement > INERTIA - 1:
-            movement -= INERTIA
-        else:
-            movement = 0
-    return (pressed, movement, movement * deltaTime)
-
-
 def main():
     global GAME_OVER
     
