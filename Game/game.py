3d78f4142c2b6aabc2fb051650f385e601accde2
Carlos Rojo <carlos.rojo@udc.es>
2024-03-11 20:32:26 +0100

    Añadido diagrama Patron Director drawio. y borrado comentarios en game.py

--- Game/game.py
+++ Game/game.py
@@ -80,39 +80,3 @@ class Game():
     def stackScene(self, scene):
         self.escena_done = True
         self.pila.append(scene)
-    
-    """
-    -------------------------------------------------------------------------
-                        CODIGO VIEJO - NO LO BORRO POR SI ACASO
-    -------------------------------------------------------------------------
-    def event_loop(self, scene):
-            if (scene.eventos(pygame.event.get())):
-                self.done = True
-            for event in pygame.event.get():
-                self.state.get_event(event)"""
-    
-    """def change_state(self):
-        next_state = self.state.next_state
-        self.state.done = False
-        self.state_name = next_state
-
-        # Datos persistentes entre estados. Ej.:(Posicion del jugador/enemigos)
-        persistent = self.state.persist
-        self.state = self.states[self.state_name]
-        self.state.startup(persistent)"""
-
-    """
-        Esta funcion checkea si hay que cambiar de estado o no
-        usando la funcion change_state.
-    """
-    """def update(self, dt):
-        if self.state.quit:
-            self.done = True
-        elif self.state.done:
-            self.change_state()
-        self.state.update(dt)
-
-        # Funcion para dibujar todo
-        def draw(self):
-            self.state.draw(self.screen)
-    """
