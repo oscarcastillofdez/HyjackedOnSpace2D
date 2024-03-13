6b91418e6ae20f28ba824ee33927f0debd70e892
Carlos Rojo <carlos.rojo@udc.es>
2024-03-13 16:54:41 +0100

    shooterEnemy sprites

--- Entities/Enemies/EnemyStates/die.py
+++ Entities/Enemies/EnemyStates/die.py
@@ -2,9 +2,10 @@ from Entities.Player.PlayerStates.base import pState
 from Game.spritesheet import Spritesheet
 
 class Die(pState):
-    def __init__(self, file, entity):
+    def __init__(self, file, entity, scale, coords, color=(0,0,0)):
         super(Die, self).__init__()
-        self.animation = Spritesheet(file,(120,120)).get_animation(0,0,24,24,4,(80,80,80))
+        x,y,w,h,n = coords
+        self.animation = Spritesheet(file,scale).get_animation(x,y,w,h,n,color)
         self.entity = entity
     
     def update(self, dt, world, player, cameraOffset,enemies_group):
